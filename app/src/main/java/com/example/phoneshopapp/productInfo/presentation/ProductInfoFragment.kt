7b33673617db09c04fd13ad39747bf8d5c4b6b9d
Eugene Ivanov <most1360@yandex.ru>
2022-07-06 20:18:50 +0300

    commit #2

--- app/src/main/java/com/example/phoneshopapp/productInfo/presentation/ProductInfoFragment.kt
+++ app/src/main/java/com/example/phoneshopapp/productInfo/presentation/ProductInfoFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.example.phoneshopapp.R
 import com.example.phoneshopapp.databinding.FragmentProductInfoBinding
 
@@ -23,8 +27,7 @@ class ProductInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProductInfoBinding.inflate(inflater, container, false)
-        adapter = ProductInfoViewPager()
-        binding.viewPager.adapter = adapter
+        initViewPager()
         productViewModel = ViewModelProvider(this)[ProductViewModel::class.java]
         productViewModel.getPhoneInfo()
         productViewModel.phoneInfo.observe(viewLifecycleOwner) { images ->
@@ -35,6 +38,21 @@ class ProductInfoFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun initViewPager() {
+        adapter = ProductInfoViewPager()
+        binding.viewPager.apply {
+            offscreenPageLimit = 1
+            val rv = getChildAt(0) as RecyclerView
+            rv.apply {
+                val padding = resources.getDimensionPixelOffset(R.dimen.pageMargin) +
+                        resources.getDimensionPixelOffset(R.dimen.pageMarginAndoffset)
+                setPadding(padding, 0, padding, 0)
+                clipToPadding = false
+            }
+        }
+        binding.viewPager.adapter = adapter
     }
 
 }
